e7c00aa5ef731db086ac01ef2124a11bddf40e8b
songwenjie <songwenjie@sensetime.com>
2022-05-13 16:11:27 +0800

    update

---
+++ test/cli.test.js
@@ -0,0 +1,7 @@
+const cli = require('../dist/index.cjs')
+
+// test('command input -c without config', () => {
+//   expect(cli.command).toBe(4);
+// });
+
+console.log(cli.command)
---
+++ test/rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from 'rollup-plugin-typescript2'
+import json from '@rollup/plugin-json'
+import path from 'path'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    { file: 'dist/index.cjs.js', format: 'cjs' },
+    { file: 'dist/index.es.js', format: 'esm' },
+  ],
+  plugins: [
+    typescript({
+      tsconfig: path.join(__dirname, 'tsconfig.json'),
+    }),
+    json(),
+  ],
+  onwarn({ code, message }) {
+    if (code !== 'UNRESOLVED_IMPORT') {
+      console.warn(message)
+    }
+  },
+}
